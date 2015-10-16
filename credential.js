e3c64f77bc487ec841781da63d97ffb8d109a3d3
lockys <locky4567@gmail.com>
2015-10-16 16:50:23 +0800

    rm credential.js

--- credential.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  account: {id: 'facebook-account', pwd: 'password',},
-  router: {toAll: '/toAll', toOffice: '/toOffice'},
-  threadID: {officeGroup: groupThreadID(Number), allGroup: allThreadID(Number)},
-  nameList: {
-    'slackbot': 'display-name-in-facebook',
-    'lockys': 'Calvin Jeng',
-  },
-};
